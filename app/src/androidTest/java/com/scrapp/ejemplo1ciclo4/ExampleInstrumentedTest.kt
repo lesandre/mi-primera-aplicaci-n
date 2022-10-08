e3a0b59f0d27accf8043380db1c23e65d806a387
Andrea Abril <leislyandreat@hotmail.com>
2022-10-07 21:21:21 -0500

    mi primera aplicacion

---
+++ app/src/androidTest/java/com/scrapp/ejemplo1ciclo4/ExampleInstrumentedTest.kt
@@ -0,0 +1,24 @@
+package com.scrapp.ejemplo1ciclo4
+
+import androidx.test.platform.app.InstrumentationRegistry
+import androidx.test.ext.junit.runners.AndroidJUnit4
+
+import org.junit.Test
+import org.junit.runner.RunWith
+
+import org.junit.Assert.*
+
+/**
+ * Instrumented test, which will execute on an Android device.
+ *
+ * See [testing documentation](http://d.android.com/tools/testing).
+ */
+@RunWith(AndroidJUnit4::class)
+class ExampleInstrumentedTest {
+    @Test
+    fun useAppContext() {
+        // Context of the app under test.
+        val appContext = InstrumentationRegistry.getInstrumentation().targetContext
+        assertEquals("com.scrapp.ejemplo1ciclo4", appContext.packageName)
+    }
+}
